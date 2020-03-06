4107612a7d6634b42be81291c268135f6fa8a9cb
Gayatri Gopavajhala <gayatri924@gmail.com>
2020-03-06 14:13:37 -0500

    Lab 06a Initial Commit Only 30 min Create original layouts and basics for first question

---
+++ app/src/main/java/com/gopavajhalagayatri/lab6/MainActivity.java
@@ -0,0 +1,25 @@
+package com.gopavajhalagayatri.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button begin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        begin = findViewById(R.id.begin);
+        begin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
