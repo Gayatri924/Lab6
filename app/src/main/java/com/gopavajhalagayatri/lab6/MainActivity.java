8f0b29a2d8a4ed944c0e5942cec4c4d3ac5201ed
Gayatri Gopavajhala <gayatri924@gmail.com>
2020-03-09 17:58:44 -0400

    Lab 06c full game is up and running with all questions --> only need to add high score and replay functionality

--- app/src/main/java/com/gopavajhalagayatri/lab6/MainActivity.java
+++ app/src/main/java/com/gopavajhalagayatri/lab6/MainActivity.java
@@ -1,6 +1,8 @@
 package com.gopavajhalagayatri.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +14,6 @@ import android.content.res.Resources;
 public class MainActivity extends AppCompatActivity {
 
     Button begin;
-    Button nextQ;
     TextView number;
     ImageView character;
     CheckBox option1;
@@ -24,19 +25,27 @@ public class MainActivity extends AppCompatActivity {
     Resources res;
     String[][] names;
     CheckBox[] boxes;
+    int[] allCharacters;
+    TextView scoring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         res = getResources();
-        names = new String[][]{res.getStringArray(R.array.one), res.getStringArray(R.array.two)};
+        names = new String[][]{res.getStringArray(R.array.one), res.getStringArray(R.array.two),
+                res.getStringArray(R.array.three), res.getStringArray(R.array.four),
+                res.getStringArray(R.array.five), res.getStringArray(R.array.six),
+                res.getStringArray(R.array.seven), res.getStringArray(R.array.eight),
+                res.getStringArray(R.array.nine), res.getStringArray(R.array.ten)};
+        allCharacters = new int[]{R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4,
+                R.drawable.image5, R.drawable.image6,R.drawable.image7, R.drawable.image8, R.drawable.image9,
+                R.drawable.image10};
         begin = findViewById(R.id.begin);
         begin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.question1);
-                nextQ = findViewById(R.id.next);
                 currentQ = 0;
                 number = findViewById(R.id.questionNum);
                 character = findViewById(R.id.image);
@@ -50,19 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 option3.setOnClickListener(x);
                 option4.setOnClickListener(x);
                 boxes = new CheckBox[]{option1, option2, option3, option4};
-                nextQ.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        currentQ += 1;
-                        if(currentQ > 1) System.out.println(score);
-                        else{
-                            String[] options = names[currentQ];
-                            for(int i = 0; i < 4; i++){
-                                boxes[i].setText(options[i]);
-                            }
-                        }
-                    }
-                });
             }
         });
     }
@@ -73,9 +69,25 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             CheckBox checkBox = (CheckBox)v;
             if(checkBox.isChecked()){
+                checkBox.setChecked(false);
                 String[] correct = res.getStringArray(R.array.correct);
-                if(checkBox.getText() ==  correct[currentQ]) score += 1;
+                if(checkBox.getText().equals(correct[currentQ])) score += 1;
+                currentQ += 1;
+                if(currentQ > 9) { //HERE
+                    setContentView(R.layout.end_quiz);
+                    scoring = findViewById(R.id.scoring);
+                    scoring.setText("Game Over! You got " + score + " points.");
+                }else{
+                    String[] options = names[currentQ];
+                    number.setText("Question "+ (currentQ + 1));
+                    character.setImageResource(allCharacters[currentQ]);
+                    for(int i = 0; i < 4; i++){
+                        boxes[i].setText(options[i]);
+                    }
+                }
             }
         }
     }
 }
+
+//Fix the restart button --> Make the high score page
