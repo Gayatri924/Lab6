2bf77a4900baf49af29f2038b33c551f16e897da
Gayatri Gopavajhala <gayatri924@gmail.com>
2020-03-09 13:22:55 -0400

    Lab 06a Change quiz type, learn how to add images, create layout for first question and how to replicate it

--- app/src/main/java/com/gopavajhalagayatri/lab6/MainActivity.java
+++ app/src/main/java/com/gopavajhalagayatri/lab6/MainActivity.java
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button begin;
+    Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         begin = findViewById(R.id.begin);
-        begin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        nextButton = findViewById(R.id.next);
+        Listener x = new Listener();
+        begin.setOnClickListener(x);
+        nextButton.setOnClickListener(x);
+    }
 
-            }
-        });
+    public class Listener implements View.OnClickListener {
 
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 }
