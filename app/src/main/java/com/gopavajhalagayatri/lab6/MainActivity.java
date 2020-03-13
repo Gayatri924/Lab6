a5a3280646b9b987770e06e49ee8080c79973803
Gayatri Gopavajhala <gayatri924@gmail.com>
2020-03-13 19:12:43 -0400

    Lab 06d and e (sorry, I forgot to break it up into seperate labs) Implement Fragment and GSON Create Leaderboard Finish App

--- app/src/main/java/com/gopavajhalagayatri/lab6/MainActivity.java
+++ app/src/main/java/com/gopavajhalagayatri/lab6/MainActivity.java
@@ -1,21 +1,27 @@
 package com.gopavajhalagayatri.lab6;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.Image;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.content.res.Resources;
+
+import com.google.gson.Gson;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button begin;
     Button replay;
     TextView number;
+    TextView first;
+    TextView second;
+    TextView third;
     ImageView character;
     CheckBox option1;
     CheckBox option2;
@@ -28,21 +34,26 @@ public class MainActivity extends AppCompatActivity {
     CheckBox[] boxes;
     int[] allCharacters;
     TextView scoring;
+    SharedPreferences sP;
+    SharedPreferences.Editor editor;
     Listener x = new Listener();
     Listener2 y = new Listener2();
+    Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         res = getResources();
+        sP = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        editor = sP.edit();
         names = new String[][]{res.getStringArray(R.array.one), res.getStringArray(R.array.two),
                 res.getStringArray(R.array.three), res.getStringArray(R.array.four),
                 res.getStringArray(R.array.five), res.getStringArray(R.array.six),
                 res.getStringArray(R.array.seven), res.getStringArray(R.array.eight),
                 res.getStringArray(R.array.nine), res.getStringArray(R.array.ten)};
         allCharacters = new int[]{R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4,
-                R.drawable.image5, R.drawable.image6,R.drawable.image7, R.drawable.image8, R.drawable.image9,
+                R.drawable.image5, R.drawable.image6, R.drawable.image7, R.drawable.image8, R.drawable.image9,
                 R.drawable.image10};
         begin = findViewById(R.id.begin);
         begin.setOnClickListener(y);
@@ -53,23 +64,47 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-            CheckBox checkBox = (CheckBox)v;
-            if(checkBox.isChecked()){
+            CheckBox checkBox = (CheckBox) v;
+            if (checkBox.isChecked()) {
                 checkBox.setChecked(false);
                 String[] correct = res.getStringArray(R.array.correct);
-                if(checkBox.getText().equals(correct[currentQ])) score += 1;
+                if (checkBox.getText().equals(correct[currentQ])) score += 1;
                 currentQ += 1;
-                if(currentQ > 9) { //HERE
+                if (currentQ > 9) { //HERE
                     setContentView(R.layout.end_quiz);
                     scoring = findViewById(R.id.scoring);
-                    scoring.setText("Game Over! You got " + score + " points.");
+                    first = findViewById(R.id.first);
+                    second = findViewById(R.id.second);
+                    third = findViewById(R.id.third);
+                    String a = sP.getString("scores", "{'one' = 0; 'two' = 0; 'three' = 0}");
+                    Leaderboard top = gson.fromJson(a, Leaderboard.class);
+                    if(score > top.one){
+                        top.three = top.two;
+                        top.two = top.one;
+                        top.one = score;
+                        scoring.setText("Game Over! You got " + score + " points. You're first on the leaderboard!");
+                    }else if(score > top.two){
+                        top.three = top.two;
+                        top.two = score;
+                        scoring.setText("Game Over! You got " + score + " points. You're second on the leaderboard!");
+                    }else if(score > top.three){
+                        top.three = score;
+                        scoring.setText("Game Over! You got " + score + " points. You're third on the leaderboard!");
+                    }else{
+                        scoring.setText("Game Over! You got " + score + " points.");
+                    }
+                    first.setText("First Place: " + top.one);
+                    second.setText("Second Place: " + top.two);
+                    third.setText("Third Place: " + top.three);
+                    editor.putString("scores", gson.toJson(top));
+                    editor.apply();
                     replay = findViewById(R.id.replay);
                     replay.setOnClickListener(y);
-                }else{
+                } else {
                     String[] options = names[currentQ];
-                    number.setText("Question "+ (currentQ + 1));
+                    number.setText("Question " + (currentQ + 1));
                     character.setImageResource(allCharacters[currentQ]);
-                    for(int i = 0; i < 4; i++){
+                    for (int i = 0; i < 4; i++) {
                         boxes[i].setText(options[i]);
                     }
                 }
@@ -97,6 +132,19 @@ public class MainActivity extends AppCompatActivity {
             boxes = new CheckBox[]{option1, option2, option3, option4};
         }
     }
-}
 
-//Fix the restart button --> Make the high score page
+    public class Leaderboard{
+        int one;
+        int two;
+        int three;
+
+        public Leaderboard(){
+        }
+
+        public Leaderboard(int first, int second, int third){
+            one = first;
+            two = second;
+            three = third;
+        }
+    }
+}
